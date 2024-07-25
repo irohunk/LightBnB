548ca25ea8fdef458a2bae1330bdef5f74c492ce
Rohan Kulkarni <irohunk@gmail.com>
2024-07-24 21:26:08 -0400

    add queries to extract data

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,10 @@
+SELECT
+reservations.id as id, start_date, title, cost_per_night,
+avg(property_reviews.rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY start_date
+LIMIT 10;
